a99b9532cbe2debdbb7008c0f55a023f4418eedf
sohey-dr <heyso1209@gmail.com>
2021-08-06 02:37:14 +0900

    [ADD]API化

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
+	"fmt"
 )
 
 func main() {
+	http.HandleFunc("/user/create", userCreate)
+	
+	http.ListenAndServe(":8080", nil)
+}
+
+func userCreate(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "Hello World from Go.")
 }
