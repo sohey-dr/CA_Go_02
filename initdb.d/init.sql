a39f43dbbf9adc152e21bc15d03eee6dff62d44c
sohey-dr <heyso1209@gmail.com>
2021-08-07 20:26:50 +0900

    [MODIFY]mod tidy

--- initdb.d/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `go_database`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(40) NOT NULL COMMENT 'ユーザーの名前',
-  `token` VARCHAR(255) NOT NULL COMMENT '認証トークン',
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
-);
