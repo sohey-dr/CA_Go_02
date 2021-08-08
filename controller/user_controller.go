83c4af73d8423fd15b0e1e6e077293626bced2be
sohey-dr <heyso1209@gmail.com>
2021-08-08 15:14:03 +0900

    [ADDGetUser追加

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -16,6 +16,10 @@ type CreateUserResponse struct {
 	Token string `json:"token"`
 }
 
+type GetUserResponse struct {
+	Name string `json:"name"`
+}
+
 func CreateUser(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
@@ -51,6 +55,44 @@ func CreateUser(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 
 	response := CreateUserResponse{}
 	response.Token = token
+	outputJson, err := json.Marshal(&response)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(outputJson))
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	if r.Method != "GET" {
+		fmt.Fprint(w, "Not get...")
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+			fmt.Println("io error")
+			return
+	}
+
+	jsonBytes := ([]byte)(body)
+	data := new(model.User)
+	if err := json.Unmarshal(jsonBytes, data); err != nil {
+			fmt.Println("JSON Unmarshal error:", err)
+			return
+	}
+
+	user := model.NewUser()
+	xToken := r.Header.Get("x-token")
+
+	db.Where("token = ?", xToken).First(&user)
+
+	response := GetUserResponse{}
+	response.Name = user.Name
 	outputJson, err := json.Marshal(&response)
 	if err != nil {
 		panic(err)
