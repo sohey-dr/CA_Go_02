804c615befc38c5b3c96bbe17b4e0d3c021a2287
soheyHashimoto <heyso1209@gmail.com>
2021-08-14 23:53:47 +0900

    [ADD]コメント追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	//  mux
 	"net/http"
 
 	"CA_Go/database"
@@ -8,20 +9,20 @@ import (
 )
 
 func main() {
+	// ここ要らない
+	// routerを作る
 	db := database.DbConnect()
 
 	// NOTE:HandleFuncを1行で書いて、第2引数のメソッドに第3引数を渡したい
 	http.HandleFunc("/user/create", func(w http.ResponseWriter, r *http.Request) {
     controller.CreateUser(w, r, db)
-  })
+	})
 	http.HandleFunc("/user/get", func(w http.ResponseWriter, r *http.Request) {
     controller.GetUser(w, r, db)
   })
 	http.HandleFunc("/user/update", func(w http.ResponseWriter, r *http.Request) {
     controller.UpdateUser(w, r, db)
   })
-
 	defer db.Close()
-
 	http.ListenAndServe(":8080", nil)
 }
