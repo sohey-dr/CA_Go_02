5e4f5b13a3dece4cb77d1dc1f1298ea3b732d74e
soheyHashimoto <heyso1209@gmail.com>
2021-08-17 08:41:32 +0900

    [ADD]user用のサブルーターを追加

--- router/router.go
+++ router/router.go
@@ -1,5 +1,20 @@
 package router
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"net/http"
+
+	"CA_Go/controller"
 )
+
+func NewRouter() error {
+	r := mux.NewRouter()
+
+	userSubRouter := r.PathPrefix("/user").Subrouter()
+	userSubRouter.HandleFunc("/create", controller.CreateUser).Methods("POST")
+	userSubRouter.HandleFunc("/get", controller.GetUser).Methods("GET")
+	userSubRouter.HandleFunc("/update", controller.UpdateUser).Methods("PUT")
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", 8080), r)
+}
