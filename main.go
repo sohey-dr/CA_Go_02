1bd02eb2637309c09f683c7c9a494b19d6408eaa
soheyHashimoto <heyso1209@gmail.com>
2021-08-17 08:41:56 +0900

    [ADD]routerの呼び出し

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	//  mux
-	"net/http"
-
-	"CA_Go/controller"
 	"CA_Go/database"
+	"CA_Go/router"
 )
 
 func main() {
@@ -13,20 +10,5 @@ func main() {
 	// routerを作る
 	database.DbConnect()
 	defer database.DB.Close()
-
-	// NOTE:HandleFuncを1行で書いて、第2引数のメソッドに第3引数を渡したい
-	http.HandleFunc("/user/create", func(w http.ResponseWriter, r *http.Request) {
-		controller.CreateUser(w, r)
-	})
-	http.HandleFunc("/user/get", func(w http.ResponseWriter, r *http.Request) {
-		controller.GetUser(w, r)
-	})
-	http.HandleFunc("/user/update", func(w http.ResponseWriter, r *http.Request) {
-		controller.UpdateUser(w, r)
-	})
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return
-	}
+	router.NewRouter()
 }
