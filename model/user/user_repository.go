23d6ec5e0cf78b40183492f49ef4c4667c1f55bf
soheyHashimoto <heyso1209@gmail.com>
2021-08-25 03:08:11 +0900

    [MOVE]メソッドを移動

--- model/user/user_repository.go
+++ model/user/user_repository.go
@@ -15,3 +15,13 @@ func Find() {
 func Update() {
 
 }
+
+func generateToken() string {
+	codeAlphabet := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, 20)
+	for i := range b {
+		b[i] = codeAlphabet[rand.Intn(len(codeAlphabet))]
+	}
+	return string(b)
+}
