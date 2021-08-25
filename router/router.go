64495848997027b93032f10c29a035cf56c9bbe2
soheyHashimoto <heyso1209@gmail.com>
2021-08-26 01:46:59 +0900

    [ADD]userパッケージを呼び出すようにする

--- router/router.go
+++ router/router.go
@@ -5,16 +5,16 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 
-	"CA_Go/controller"
+	"CA_Go/controller/user"
 )
 
 func NewRouter() error {
 	r := mux.NewRouter()
 
 	userSubRouter := r.PathPrefix("/user").Subrouter()
-	userSubRouter.HandleFunc("/create", controller.CreateUser).Methods("POST")
-	userSubRouter.HandleFunc("/get", controller.GetUser).Methods("GET")
-	userSubRouter.HandleFunc("/update", controller.UpdateUser).Methods("PUT")
+	userSubRouter.HandleFunc("/create", user.CreateUser).Methods("POST")
+	userSubRouter.HandleFunc("/get", user.GetUser).Methods("GET")
+	userSubRouter.HandleFunc("/update", user.UpdateUser).Methods("PUT")
 
 	return http.ListenAndServe(fmt.Sprintf(":%d", 8080), r)
 }
